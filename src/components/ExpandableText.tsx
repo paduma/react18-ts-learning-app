d3764be8fccabbaab853a88672f572b3a14f7d3c
paduma <paduma625@gmail.com>
2024-11-05 15:02:09 +0800

    ExpandableText component

---
+++ src/components/ExpandableText.tsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+interface Props {
+  children: string;
+  maxChars?: number;
+}
+
+export const ExpandableText = ({ children, maxChars = 100 }: Props) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  if (children.length <= maxChars) return <p>{children}</p>;
+  const text = isExpanded ? children : children.substring(0, maxChars);
+  return (
+    <p>
+      {text}...
+      <button onClick={() => setIsExpanded(!isExpanded)}>
+        {isExpanded ? "Less" : "More"}
+      </button>
+    </p>
+  );
+};
