3b33bcc4b907eef2c9445bdb338bc26f0bd6d33c
paduma <paduma625@gmail.com>
2024-11-05 17:55:56 +0800

    mini app expenses-tracker

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
-import Form from "./components/Form";
+import { useState } from "react";
+import ExpenseList from "./expense-tracker/components/ExpenseList";
+import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
+import "./App.css";
 
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "ad", amount: 10, category: "Utilities" },
+    { id: 2, description: "abd", amount: 10, category: "Utilities" },
+    { id: 3, description: "acd", amount: 10, category: "Utilities" },
+    { id: 4, description: "add", amount: 10, category: "Utilities" },
+  ]);
+
+  const visibleExpenses = selectedCategory
+    ? expenses.filter((e) => e.category === selectedCategory)
+    : expenses;
   return (
     <>
-      <Form></Form>
+      <div className="mb-3">
+        <ExpenseFilter
+          onSelectCategory={(category) => setSelectedCategory(category)}
+        ></ExpenseFilter>
+      </div>
+      <ExpenseList
+        expenses={visibleExpenses}
+        onDelete={(id) =>
+          setExpenses(expenses.filter((item) => item.id !== id))
+        }
+      ></ExpenseList>
     </>
   );
 }
