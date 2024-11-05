6def7d56e0991d75013b7a23535f17f75567415e
paduma <paduma625@gmail.com>
2024-11-05 12:00:46 +0800

    using immer update array nested in object

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
-import ListGroup from "./components/ListGroup";
+import { useState } from "react";
+import { produce } from "immer";
 
 function App() {
-  let items = ["Beijing", "New York", "London"];
+  const [bugs, setBugs] = useState([
+    { id: 1, title: "Bug 1", fixed: false },
+    { id: 2, title: "Bug 2", fixed: true },
+  ]);
+
+  const handleClick = () => {
+    setBugs(
+      produce((draft) => {
+        const bug = draft.find((bug) => bug.id === 1);
+        if (bug) bug.fixed = true;
+      })
+    );
+  };
+
   return (
     <>
-      <ListGroup items={items} heading="ListGroup" onSelectItems={() => {}} />
+      {bugs.map((bug) => (
+        <p key={bug.id}>
+          {bug.title} {bug.fixed ? "Fixed" : "New"}
+        </p>
+      ))}
+      <button onClick={handleClick}>Click Me</button>
     </>
   );
 }
